60331b2aaf27abec65de9592bec5cda0f5b091ea
Kethan Pilla <90024966+KethanPilla@users.noreply.github.com>
2024-10-23 23:54:48 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  
+  $('#reset').on('click', function(){
+    //move my doge back to the start 
+    $("#dogePic").css( 'top', '0px').css('left', '0px');
+    //https://jqueryui.com/position/
+  })
+  
+  $("img").draggable( {
+      stop: function() {
+       // alert('dragged'); 
+        
+        if ( $("img").position().left > 100)
+          {
+            //they won
+            $("body").append("<h2>YOU WON</h2> ") 
+          }
+        // alert( $("img").position().left ); 
+        
+ 
+      }
+  });  
+ 
+  
+  
+});
+
